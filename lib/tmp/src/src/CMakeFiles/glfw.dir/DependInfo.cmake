adff97d047c1fc0d658262d0aa8ee3ffd213b47a
Jules <jloro@student.42.fr>
2019-04-13 14:24:22 +0200

    remove submodule glfw

---
+++ lib/tmp/src/src/CMakeFiles/glfw.dir/DependInfo.cmake
@@ -0,0 +1,42 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/jules/scop/lib/glfw/src/cocoa_init.m" "/Users/jules/scop/lib/glfw/src/src/CMakeFiles/glfw.dir/cocoa_init.m.o"
+  "/Users/jules/scop/lib/glfw/src/cocoa_joystick.m" "/Users/jules/scop/lib/glfw/src/src/CMakeFiles/glfw.dir/cocoa_joystick.m.o"
+  "/Users/jules/scop/lib/glfw/src/cocoa_monitor.m" "/Users/jules/scop/lib/glfw/src/src/CMakeFiles/glfw.dir/cocoa_monitor.m.o"
+  "/Users/jules/scop/lib/glfw/src/cocoa_time.c" "/Users/jules/scop/lib/glfw/src/src/CMakeFiles/glfw.dir/cocoa_time.c.o"
+  "/Users/jules/scop/lib/glfw/src/cocoa_window.m" "/Users/jules/scop/lib/glfw/src/src/CMakeFiles/glfw.dir/cocoa_window.m.o"
+  "/Users/jules/scop/lib/glfw/src/context.c" "/Users/jules/scop/lib/glfw/src/src/CMakeFiles/glfw.dir/context.c.o"
+  "/Users/jules/scop/lib/glfw/src/egl_context.c" "/Users/jules/scop/lib/glfw/src/src/CMakeFiles/glfw.dir/egl_context.c.o"
+  "/Users/jules/scop/lib/glfw/src/init.c" "/Users/jules/scop/lib/glfw/src/src/CMakeFiles/glfw.dir/init.c.o"
+  "/Users/jules/scop/lib/glfw/src/input.c" "/Users/jules/scop/lib/glfw/src/src/CMakeFiles/glfw.dir/input.c.o"
+  "/Users/jules/scop/lib/glfw/src/monitor.c" "/Users/jules/scop/lib/glfw/src/src/CMakeFiles/glfw.dir/monitor.c.o"
+  "/Users/jules/scop/lib/glfw/src/nsgl_context.m" "/Users/jules/scop/lib/glfw/src/src/CMakeFiles/glfw.dir/nsgl_context.m.o"
+  "/Users/jules/scop/lib/glfw/src/osmesa_context.c" "/Users/jules/scop/lib/glfw/src/src/CMakeFiles/glfw.dir/osmesa_context.c.o"
+  "/Users/jules/scop/lib/glfw/src/posix_thread.c" "/Users/jules/scop/lib/glfw/src/src/CMakeFiles/glfw.dir/posix_thread.c.o"
+  "/Users/jules/scop/lib/glfw/src/vulkan.c" "/Users/jules/scop/lib/glfw/src/src/CMakeFiles/glfw.dir/vulkan.c.o"
+  "/Users/jules/scop/lib/glfw/src/window.c" "/Users/jules/scop/lib/glfw/src/src/CMakeFiles/glfw.dir/window.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "_GLFW_USE_CONFIG_H"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../include"
+  "."
+  "src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
